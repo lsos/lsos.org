da530e044b203da1971bec477ff2be574e61d0da
brillout <github.com@brillout.com>
2021-11-19 20:28:28 +0000

    Deploying to gh-pages from @ lsos/lsos.org@dda4ef2b2fa3d7234855eaca57188e9ca0ae7c93 🚀

---
+++ assets/index.page.6150ff81.js
@@ -0,0 +1,18 @@
+var d=Object.defineProperty,f=Object.defineProperties;var h=Object.getOwnPropertyDescriptors;var o=Object.getOwnPropertySymbols;var s=Object.prototype.hasOwnProperty,l=Object.prototype.propertyIsEnumerable;var c=(n,t,a)=>t in n?d(n,t,{enumerable:!0,configurable:!0,writable:!0,value:a}):n[t]=a,r=(n,t)=>{for(var a in t||(t={}))s.call(t,a)&&c(n,a,t[a]);if(o)for(var a of o(t))l.call(t,a)&&c(n,a,t[a]);return n},p=(n,t)=>f(n,h(t));var u=(n,t)=>{var a={};for(var i in n)s.call(n,i)&&t.indexOf(i)<0&&(a[i]=n[i]);if(n!=null&&o)for(var i of o(n))t.indexOf(i)<0&&l.call(n,i)&&(a[i]=n[i]);return a};import"./vendor.4ff7423c.js";import{c as e}from"./esm.f9af9176.js";import{p as g}from"./page_config.2cbfe3eb.js";const b={},v="wrapper";function m(a){var i=a,{components:n}=i,t=u(i,["components"]);return e(v,p(r(r({},b),t),{components:n,mdxType:"MDXLayout"}),e("h2",null,"Our Pledge"),e("p",null,`In the interest of fostering an open and welcoming environment, we
+pledge to making participation in the Lsos and
+our community a constructive experience for everyone.`),e("h2",null,"Our Standards"),e("p",null,`Examples of behavior that contributes to creating a positive environment
+include:`),e("ul",null,e("li",{parentName:"ul"},"Being respectful of differing viewpoints and experiences"),e("li",{parentName:"ul"},"Gracefully accepting constructive criticism"),e("li",{parentName:"ul"},"Focusing on what is best for the community"),e("li",{parentName:"ul"},"Showing empathy towards other community members")),e("p",null,"Examples of unacceptable behavior by participants include:"),e("ul",null,e("li",{parentName:"ul"},"Insulting/derogatory comments and personal attacks"),e("li",{parentName:"ul"},"Harassment"),e("li",{parentName:"ul"},"Publishing others' private information")),e("h2",null,"Our Responsibilities"),e("p",null,`We are responsible for clarifying the standards of acceptable
+behavior and we take appropriate and fair corrective action in
+response to any instances of unacceptable behavior.`),e("p",null,`We have the right and responsibility to remove, edit, or
+reject comments, commits, code, wiki edits, issues, and other contributions
+that are not aligned to this Code of Conduct, or to ban temporarily or
+permanently any contributor for other behaviors that we deem inappropriate,
+threatening, offensive, or harmful.`),e("h2",null,"Scope"),e("p",null,`This Code of Conduct applies both within Lsos spaces and in public spaces
+when an individual is representing the Lsos or its community. Examples of
+representing the Lsos or community include using an official Lsos e-mail
+address, posting via an official social media account, or acting as an appointed
+representative at an online or offline event.`),e("h2",null,"Enforcement"),e("p",null,`Instances of abusive, harassing, or otherwise unacceptable behavior can be
+reported by contacting us at `,e("a",{className:"contact-address","data-recipient":"conduct"}),`.
+All complaints will be reviewed and investigated and will result in a response that
+is deemed necessary and appropriate to the circumstances.
+We maintain confidentiality with regard to the reporter of an incident.`))}m.isMDXComponent=!0;var N=g(m,"Code of Conduct");export{N as default};
---
+++ assets/index.page.ffd7ea5e.js
@@ -0,0 +1,21 @@
+var d=Object.defineProperty,h=Object.defineProperties;var m=Object.getOwnPropertyDescriptors;var i=Object.getOwnPropertySymbols;var r=Object.prototype.hasOwnProperty,c=Object.prototype.propertyIsEnumerable;var l=(e,n,o)=>n in e?d(e,n,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[n]=o,s=(e,n)=>{for(var o in n||(n={}))r.call(n,o)&&l(e,o,n[o]);if(i)for(var o of i(n))c.call(n,o)&&l(e,o,n[o]);return e},p=(e,n)=>h(e,m(n));var u=(e,n)=>{var o={};for(var t in e)r.call(e,t)&&n.indexOf(t)<0&&(o[t]=e[t]);if(e!=null&&i)for(var t of i(e))n.indexOf(t)<0&&c.call(e,t)&&(o[t]=e[t]);return o};import"./vendor.4ff7423c.js";import{c as a}from"./esm.f9af9176.js";import{p as g}from"./page_config.2cbfe3eb.js";const y={},b="wrapper";function f(o){var t=o,{components:e}=t,n=u(t,["components"]);return a(b,p(s(s({},y),n),{components:e,mdxType:"MDXLayout"}),a("p",null,`In the short term,
+we design financial solutions for
+open source projects that struggle to become financially sustainable and independent.
+In the long term,
+we envision the ubiquity of open source as new financial models emerge.
+We belive in a future of financially sustainable open source alternatives to most software including Facebook, Zoom, or AWS.`),a("p",null,`The open source movement originated in the late '90s from a longing for a trade-off between the Free Software Fundation (FSF) uncompromising vision and a more practical and commercially-minded stance.
+We sympathize and espouse FSF's vision of a society of freedom, but
+we believe that permissive licenses and bringing open source and financial values together is the next step to achieve FSF's vision.`),a("p",null,`Photoshop is a proprietary software and yet has had a substantial impact in the graphic design industry and
+virtually all graphic designers swear only by Photoshop.
+Not despite but because of being proprietary;
+giving it the financial resources it needs.
+In a sense, we can be glad that Photoshop went proprietary,
+as open source alternatives fall short in comparison `,"\u2014",` they simply don't have the same financial resources.
+We design
+financial solutions to bring together open source and the financial needs of elaborate software such as Photoshop.`),a("p",null,`Open source is of crucial importance for
+critical infrastructure code
+such as operating systems.
+Infrastructure code is under constant attack of
+oppressive governments and selfish interests, and open source is paramount in mitigating these threats.
+We design
+financial solutions to ensure the financial independence of infrastructure code.`))}f.isMDXComponent=!0;var P=g(f,"Mission");export{P as default};
